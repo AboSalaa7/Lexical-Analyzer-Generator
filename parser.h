43e7709a1c2664fafcef5524b951a75c0153794d
linhahmed <45941034+linhahmed@users.noreply.github.com>
2021-05-30 12:21:11 +0200

    Add files via upload

---
+++ parser.h
@@ -0,0 +1,54 @@
+
+
+#ifndef LEXICAL_AANALYZER_GENERATOR_PARSER_H
+#define LEXICAL_AANALYZER_GENERATOR_PARSER_H
+#include "NFA.h"
+#include "DFA.h"
+
+class parser
+{
+public:
+    parser(string File, string opFile);
+    NFA* getFullNFA();
+    NFA* clone(NFA* g);
+    vector<string> get_weights();
+    vector<string> get_expressions();
+
+    vector<string> getOpFile();
+    void readOpFile();
+
+    virtual ~parser();
+
+
+protected:
+
+private:
+    string RegexFile;
+    string opFile;
+    vector<string> Regex;
+    int statenum = 0;
+    unordered_map<string,NFA> definitions;
+    unordered_map<string,NFA> expressions;
+    vector<string> expNames;
+    vector<string> weights;
+
+    vector<string> opFileRegex;
+
+    void getNFA(string line);
+    void readRegexFile(string path);
+    void addOperation(char c, stack<string>* in, stack<string>* post);
+    bool isOperation(char c);
+    bool isOperation(string c);
+    string removeSpaces(string str);
+    vector<string> getTokens(string str2);
+    stack<string> getPostfix(vector<string> tokens);
+    bool isBinaryOp(string c);
+
+    string parseToken(string token);
+    DFA dfa;
+
+};
+
+
+
+#endif //LEXICAL_AANALYZER_GENERATOR_PARSER_H
