6ccd8fe39d11a85f366931895555b2552bf94563
linhahmed <45941034+linhahmed@users.noreply.github.com>
2021-06-04 19:46:09 +0200

    #file_parser

---
+++ Classes/CFGParser.h
@@ -0,0 +1,30 @@
+#ifndef CFGPARSER_H
+#define CFGPARSER_H
+#include <bits/stdc++.h>
+#include <iostream>
+#include <regex>
+#include <stack>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+struct prule {
+    string lhs;
+    vector<vector<string>> rhs;
+};
+
+class CFGParser
+{
+    public:
+        CFGParser(string file);
+        void parseFile();
+
+        virtual ~CFGParser();
+
+    private:
+        string grammerFile;
+
+        string trim(const string &s);
+};
+
+#endif // CFGPARSER_H
